84f55b8d5a4c6f94262fa303035f0f6bb3d3c48a
Kylar <kylarrufus005@gmail.com>
2023-03-30 10:43:30 +0200

    Store entity

---
+++ src/main/java/org/example/entities/Customer.java
@@ -0,0 +1,112 @@
+package org.example.entities;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+
+public class Customer {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "customer_id")
+    private int customerId;
+
+    @Column(name = "store_id", nullable = false)
+    private int storeId;
+
+    @Column(length = 50, nullable = false)
+    private String firstName;
+
+    @Column(length = 50, nullable = false)
+    private String lastName;
+
+    @Column(length = 50, nullable = false)
+    private String email;
+
+    @Column(name = "address_id", nullable = false)
+    private int addressId;
+
+    @Column(name = "active")
+    private byte active;
+
+    @Column(name = "create_date")
+    private Timestamp createDate;
+
+    @Column(name = "last_update")
+    private Timestamp lastUpdate;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public byte getActive() {
+        return active;
+    }
+
+    public void setActive(byte active) {
+        this.active = active;
+    }
+
+    public Timestamp getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Timestamp createDate) {
+        this.createDate = createDate;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+
+}
