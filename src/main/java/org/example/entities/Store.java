e1fb248319ef3df6b0268787fd97e1df9e88a940
Kylar <kylarrufus005@gmail.com>
2023-03-30 10:27:39 +0200

    Store entity

---
+++ src/main/java/org/example/entities/Store.java
@@ -0,0 +1,57 @@
+package org.example.entities;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+
+public class Store {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "store_id")
+    private int storeId;
+
+    @Column(name = "manager_staff_id", nullable = false)
+    private int managerStaffId;
+
+    @Column(name = "address_id", nullable = false)
+    private int addressId;
+
+    @Column(name = "last_update")
+    private Timestamp lastUpdate;
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+
+    public int getManagerStaffId() {
+        return managerStaffId;
+    }
+
+    public void setManagerStaffId(int managerStaffId) {
+        this.managerStaffId = managerStaffId;
+    }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+
+}
