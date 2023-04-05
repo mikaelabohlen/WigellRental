41641900a1ed3ffdbf297c5c81c336e8acd1b395
Caroline Franzén <caroline.billstrom@gmail.com>
2023-04-05 16:27:53 +0200

    fetchActors fixed

--- src/main/java/org/example/dao/ActorDAO.java
+++ src/main/java/org/example/dao/ActorDAO.java
@@ -1,6 +1,7 @@
 package org.example.dao;
 
 import org.example.entities.Actor;
+import org.example.entities.Film;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
@@ -17,6 +18,17 @@ public class ActorDAO extends AbstractDAO<Actor> {
             Query<Actor> query = session.createQuery("from Actor where firstName = :firstName", Actor.class);
             query.setParameter("firstName", firstName);
             return query.getResultList();
+        }
+    }
+
+    public List<Actor> getActorsForFilm(int selectedFilmId) {
+        try (Session session = getSession()) {
+            session.beginTransaction();
+            Query query= session.createQuery("SELECT a FROM Actor a INNER JOIN a.films f WHERE f.id = :filmId");
+            query.setParameter("filmId", selectedFilmId);
+            List<Actor> actors = query.getResultList();
+            session.getTransaction().commit();
+            return actors;
         }
     }
 }
